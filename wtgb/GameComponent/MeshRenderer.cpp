c8d005d3afba33564c8e73f068262d3169052cee
3doriTea <sorasora3program@gmail.com>
2025-12-02 13:14:32 +0900

    インプットレイアウトのコメントﾎﾝﾉｽｺｼ整形

--- wtgb/GameComponent/MeshRenderer.cpp
+++ wtgb/GameComponent/MeshRenderer.cpp
@@ -30,7 +30,7 @@ void wtgb::MeshRenderer::Init(ViewerCached _system)
 			.vertexInputLayout
 			{
 				{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0,  D3D11_INPUT_PER_VERTEX_DATA, 0 },   // 位置
-				{ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 16, D3D11_INPUT_PER_VERTEX_DATA, 0 },  // 法線ベクトル
+				{ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 16, D3D11_INPUT_PER_VERTEX_DATA, 0 },     // 法線ベクトル
 				{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 32, D3D11_INPUT_PER_VERTEX_DATA, 0 },      // UV座標
 			},
 			.fillMode = D3D11_FILL_SOLID,  // 塗りつぶし
