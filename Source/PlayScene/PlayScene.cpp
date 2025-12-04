4738c353e3cb909055ccb46fcce1c48d1183cb16
3doriTea <sorasora3program@gmail.com>
2025-12-04 12:05:37 +0900

    テスト用球生成

--- Source/PlayScene/PlayScene.cpp
+++ Source/PlayScene/PlayScene.cpp
@@ -31,9 +31,9 @@ void PlayScene::Start()
 
 	//Instantiate<TestBillBoard>();
 	
-	//EntityId pRootPlayer{ Instantiate<Player>(INVALID_ENTITY, Vector3{ 2.5f, 5.0f, 5.0f }) };
+	EntityId pRootPlayer{ Instantiate<Player>(INVALID_ENTITY, Vector3{ 2.5f, 5.0f, 5.0f }) };
 	//EntityId pRootPlayer{ Instantiate<Player>(INVALID_ENTITY, Vector3{ 2.5f, -180.0f, 300.0f }) };
-	EntityId pRootPlayer{ Instantiate<Player>(INVALID_ENTITY, Vector3{ 2.5f, -180.0f, 300.0f }) };
+	//EntityId pRootPlayer{ Instantiate<Player>(INVALID_ENTITY, Vector3{ 2.5f, -180.0f, 300.0f }) };
 	/*for (int k = 0; k < 3; k++)
 	{
 		for (int j = 0; j < 3; j++)
@@ -48,11 +48,11 @@ void PlayScene::Start()
 	Mathf::Randomer random{};
 
 #if 1
-	for (int i = 0; i < 0; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		float r1{ random.Rand() };
 		float r2{ random.Rand() };
-		Instantiate<WaterSphere>(Vector3{ r1 * 300.0f + 50.0f, 5.0f, r2 * 10.0f });
+		Instantiate<WaterSphere>(Vector3{ r1 * 300.0f + 50.0f, 5.0f, r2 * 100.0f + 100.0f });
 	}
 #else
 	for (int i = 0; i < 100; i++)
