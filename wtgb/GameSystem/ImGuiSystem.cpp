81d0e871d8670a44a8d1979e11164c7cb93297a3
3doriTea <sorasora3program@gmail.com>
2025-12-12 18:36:41 +0900

    ImGuiシステムを追加中

---
+++ wtgb/GameSystem/ImGuiSystem.cpp
@@ -0,0 +1,57 @@
+#include "pch\pch.h"
+#include "ImGuiSystem.h"
+#include "GameWindow.h"
+#include "Direct3D.h"
+#include "Direct3D/Direct3DResource.h"
+
+wtgb::ImGuiSystem::ImGuiSystem() :
+	firstFrame_{ true }
+{
+}
+
+wtgb::ImGuiSystem::~ImGuiSystem()
+{
+}
+
+wtgb::Result wtgb::ImGuiSystem::Init(const ViewerInit& _system)
+{
+	GameWindow& gameWindow{ _system.Get<GameWindow>() };
+	Direct3DResource& direct3DResource{ _system.Get<Direct3D>().Resource() };
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io{ ImGui::GetIO() };
+	(void)io;  // ç≈ìKâªÇ≈è¡Ç≥ÇÍÇÈÇÃÇñhé~
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+
+	ImGui::StyleColorsLight();
+
+	ImGuiStyle& style{ ImGui::GetStyle() };
+	//style.ScaleAllSizes();
+	//style.FontScaleDpi();
+
+	ImGui_ImplWin32_Init(gameWindow.GetMainWindowHandle());
+	ImGui_ImplDX11_Init(direct3DResource.Device().Get(), direct3DResource.Context().Get());
+
+	return Result::Code::Ok;
+}
+
+void wtgb::ImGuiSystem::Update(const ViewerUpdate& _system)
+{
+	if (firstFrame_ == false)
+	{
+		ImGui::Render();
+	}
+
+	ImGui_ImplDX11_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
+}
+
+void wtgb::ImGuiSystem::End()
+{
+	ImGui_ImplDX11_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
+}
