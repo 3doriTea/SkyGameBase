59d829139d777d70c4388b44359ae5cb9bb3d929
3doriTea <sorasora3program@gmail.com>
2025-12-13 20:22:00 +0900

    setting.iniにウィンドウ設定を移した。例の謎バグは未開のままクリーンビルドで直ってしまった。VSを３つ立ち上げていたのが原因の可能性大。

--- Source/Systems/MainWindow.cpp
+++ Source/Systems/MainWindow.cpp
@@ -17,9 +17,17 @@ MainWindow::~MainWindow()
 
 wtgb::Result MainWindow::Init(const ViewerInit& _viewer)
 {
+	const char* SETTING_FILE_NAME{ "./setting.ini" };
+
+	const uint32_t WINDOW_WIDTH       { GetPrivateProfileInt("Window", "Width", 1600, SETTING_FILE_NAME) };
+	const uint32_t WINDOW_HEIGHT      { GetPrivateProfileInt("Window", "Height", 900, SETTING_FILE_NAME) };
+	const uint32_t WINDOW_INIT_POS_X  { GetPrivateProfileInt("Window", "InitPosX", 50, SETTING_FILE_NAME) };
+	const uint32_t WINDOW_INIT_POS_Y  { GetPrivateProfileInt("Window", "InitPosY", 50, SETTING_FILE_NAME) };
+	const uint32_t WINDOW_REFRESH_RATE{ GetPrivateProfileInt("Window", "RefreshRate", 60, SETTING_FILE_NAME) };
+
 	GameWindowHandle hGameWindow
 	{
-		_viewer.Get<GameWindow>().Create(
+		_viewer.Get<GameWindow>().Create(GameWindow::CreateWindowConfig
 		{
 			.title = Game::Title(),
 			.classStyle = CS_VREDRAW | CS_HREDRAW,
@@ -29,11 +37,10 @@ wtgb::Result MainWindow::Init(const ViewerInit& _viewer)
 			.clientStyle = WS_OVERLAPPEDWINDOW,
 			.clientStyleEx = WS_EX_OVERLAPPEDWINDOW,
 			.hasMenu = FALSE,
-			// TODO: スクリーンサイズをinitファイルから読み込む
-			.windowScreenSize = { 1600, 900 },
-			.initPosition = { 50, 50 },
+			.windowScreenSize = { static_cast<int32_t>(WINDOW_WIDTH), static_cast<int32_t>(WINDOW_HEIGHT) },
+			.initPosition = { static_cast<int32_t>(WINDOW_INIT_POS_X), static_cast<int32_t>(WINDOW_INIT_POS_Y) },
 			.hWndParent = nullptr,
-			.refreshRateSec = 60
+			.refreshRateSec = WINDOW_REFRESH_RATE,
 		})
 	};
 
