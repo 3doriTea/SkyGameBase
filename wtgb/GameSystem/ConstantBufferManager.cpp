7e6cefb014e6bcc6089bcfee909df0f1af7a0057
3doriTea <sorasora3program@gmail.com>
2026-02-05 10:37:59 +0900

    ConstantBufferManagerのcpp追加

---
+++ wtgb/GameSystem/ConstantBufferManager.cpp
@@ -0,0 +1,22 @@
+#include "ConstantBufferManager.h"
+
+wtgb::ConstantBufferManager::ConstantBufferManager()
+{
+}
+
+wtgb::ConstantBufferManager::~ConstantBufferManager()
+{
+}
+
+Result wtgb::ConstantBufferManager::Init(const ViewerInit& _viewer)
+{
+    return Result();
+}
+
+void wtgb::ConstantBufferManager::Update(const ViewerUpdate& _viewer)
+{
+}
+
+void wtgb::ConstantBufferManager::End()
+{
+}
