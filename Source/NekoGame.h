ae8d2169b929247189309d8734b634b4f2bdb7b6
3doriTea <sorasora3program@gmail.com>
2026-04-15 19:49:18 +0900

    リリース用に分けた

--- Source/NekoGame.h
+++ Source/NekoGame.h
@@ -11,7 +11,7 @@ public:
 	NekoGame() {}
 	~NekoGame() {}
 
-	inline const char8_t* GetTitle() const override { return u8"はねねこ ゲーム"; }
+	inline const char8_t* GetTitle() const override { return u8"はねねこ ボール"; }
 	inline const char8_t* GetVersion() const override { return u8"1.0.1"; }
 
 	void StartRegister(const GameSystemCollection::GameSystemAdder& _pGameSystemAdder) override;
