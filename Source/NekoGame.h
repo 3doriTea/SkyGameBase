385448ac01ceeb2239e68e919c656b830f379da3
3doriTea <sorasora3program@gmail.com>
2026-05-24 19:28:42 +0900

    コメント追記

--- Source/NekoGame.h
+++ Source/NekoGame.h
@@ -11,8 +11,20 @@ public:
 	NekoGame() {}
 	~NekoGame() {}
 
+	/// <summary>
+	/// ゲームのタイトルを取得する
+	/// </summary>
+	/// <returns>ゲームのタイトル</returns>
 	inline const char8_t* GetTitle() const override { return u8"はねねこ ボール"; }
+	/// <summary>
+	/// ゲームのバージョンを取得する
+	/// </summary>
+	/// <returns>ゲームのバージョン</returns>
 	inline const char8_t* GetVersion() const override { return u8"1.1.2"; }
 
+	/// <summary>
+	/// 開始時のゲームシステム登録
+	/// </summary>
+	/// <param name="_pGameSystemAdder">ゲームシステムコレクションへの追加参照</param>
 	void StartRegister(const GameSystemCollection::GameSystemAdder& _pGameSystemAdder) override;
 };
